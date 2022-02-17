8e25bc9fe615d180e1f6b371e7786beb46325d8e
Edwin Hernandez <Edwinhernandez2@my.unt.edu>
2022-02-17 12:07:17 -0600

    added dependcies and updated routes

--- bstFront/src/app/app.module.ts
+++ bstFront/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { FlashMessagesModule } from 'flash-messages-angular';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// Importing Components (Website Pages)
+import { HomeComponent } from './components/home/home.component';
 
+const appRoutes =[
+  { path: '', component: HomeComponent},
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    AppRoutingModule,
+    FlashMessagesModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
